513e34f622a1cf70958f66b8d127ae57f11c0351
Tony Bowden <tony@tmtm.com>
2017-04-02 10:09:59 +0100

    Change source URL
    The list that was on the Period page has been removed, and now points at
    the election results.

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,13 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
-end
-
-url = 'https://es.wikipedia.org/wiki/Segundo_período_legislativo_de_la_Asamblea_Nacional_de_Ecuador'
-colname = 'Asambleísta'
-
-noko = noko_for(url)
-names = noko.xpath('//table[.//th[.="%s"]]' % colname).map do |table|
-  table.css('th').map(&:text).map.with_index { |e, i| i if e == colname }.compact.map { |col|
-    table.xpath('.//td[%s]//a[not(@class="new")]/@title' % (col+1).to_s) 
-  }
-end.flatten.map(&:text)
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://es.wikipedia.org/wiki/Elecciones_legislativas_de_Ecuador_de_2013',
+  after: '//span[@id="N.C3.B3mina_de_asamble.C3.ADstas_electos"]',
+  before: '//span[@id="V.C3.A9ase_tambi.C3.A9n"]',
+  xpath: './/a[not(@class="new")]/@title',
+).reject { |n| n.start_with? 'Editar' }
 
 ids = %w(Q29051867)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { es: names.uniq })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { es: names })
